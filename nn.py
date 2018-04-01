c2d4281462d69105b33037d6c617102aa59f09ee
Jason Wang <ycwanglovecake@gmail.com>
2018-04-01 16:59:25 -0400

    fix bugs

--- nn.py
+++ nn.py
@@ -23,29 +23,30 @@ print(y_train_onehot.shape)
 model = Sequential()
 model.add(Dense(
 			units=93, 
-			input_shape=X_train.shape,
+			input_dim=X_train.shape[1],
 			activation='relu'))
 model.add(Dense(
 			units=9, 
-			input_shape=X_train.shape,
-			activation='softmas'))
+			input_dim=X_train.shape[1],
+			activation='softmax'))
 
 sgd = SGD(lr=0.001, decay=1e-7, momentum=.9)
 model.compile(
 			optimizer=sgd,
-			loss='catagorical_crossentropy')
+			loss='categorical_crossentropy')
 
-history = model.fit(X_train, y_train_onehot, batch_size=64, epochs=20,
+history = model.fit(X_train, y_train_onehot, batch_size=64, epochs=50,
 					verbose=1, validation_split=0.1)
 
 y_test_pred = model.predict_classes(X_test,
 									verbose=0)
 y_test_pred = le.inverse_transform(y_test_pred)
 
-correct_preds = np.sum(y_test_pred == y_test, axis=0)
+print(y_test_pred)
+correct_preds = np.sum(y_test_pred == y_test)
 test_acc = correct_preds / y_test.shape[0]
 
-print("Test accuracy: %.2f%%" (test_acc * 100))
+print("Test accuracy: %.2f%%" %(test_acc * 100))
 
 
 
